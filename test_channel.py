fab7ab27f9082d55586bebac20de3e1f1fe50e9a
AnnaPenkinaPython <annapenkina200121@mail.ru>
2023-03-03 14:51:31 +0700

    somthng changed

--- test_channel.py
+++ test_channel.py
@@ -5,7 +5,7 @@ import pytest
 
 def test_repr():
     channel_id = Youtube("UCMCgOm8GZkHp8zJ6l7_hIuA")
-    assert channel_id.__repr__ == "Youtube-channel: вДудь"
+    assert channel_id.__repr__() == "Youtube-channel: вДудь"
 
 def test_add():
     channel_id = Youtube("UCByhZ-JEe5OOZSuq0uaXOng")

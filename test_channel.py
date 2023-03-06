6f83ea1fe88bd3ee9ba3d0ae151d5adc08411ae9
AnnaPenkinaPython <annapenkina200121@mail.ru>
2023-03-06 16:05:39 +0700

    somthng changed

--- test_channel.py
+++ test_channel.py
@@ -2,17 +2,18 @@ from main import Youtube
 import pytest
 
 
-
 def test_str():
     channel_id = Youtube("UCMCgOm8GZkHp8zJ6l7_hIuA")
     assert channel_id.__str__() == "Youtube-channel: вДудь"
+
 
 def test_add():
     channel_id = Youtube("UCByhZ-JEe5OOZSuq0uaXOng")
     other_id = Youtube("UCMCgOm8GZkHp8zJ6l7_hIuA")
     assert channel_id + other_id == "98900010300000"
 
+
 def test_lt():
     channel_id = Youtube("UCByhZ-JEe5OOZSuq0uaXOng")
     other_id = Youtube("UCMCgOm8GZkHp8zJ6l7_hIuA")
-    assert channel_id.subscriber_count > other_id.subscriber_count == '10300000'
+    assert (channel_id.subscriber_count > other_id.subscriber_count) is True

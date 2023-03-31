d6b7e67b8d2d60a3def257317a7e1f98cb34f074
AnnaPenkinaPython <annapenkina200121@mail.ru>
2023-04-01 00:20:13 +0700

    project changed

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ class Video:
             self.title = self.video_response['items'][0]['snippet']['title']
             self.view_count = self.video_response['items'][0]['statistics']['viewCount']
             self.like_count = self.video_response['items'][0]['statistics']['likeCount']
-        except Exception:
+        except IndexError:
             self.video_response = None
             self.title = None
             self.view_count = None
